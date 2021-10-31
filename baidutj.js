b9a52ece99fb486072586693ae277922a932fe46
baidu.com <76475436+x8x8vip@users.noreply.github.com>
2021-10-31 01:22:43 -0700

    Add files via upload

---
+++ baidutj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?38cffc8371535b675417040e955427de";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
